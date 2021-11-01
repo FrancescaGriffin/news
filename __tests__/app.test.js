9c88ab9d0778ea0470bde8baa496cf89e089a560
Francesca Kissack <francesca.kissack@gmail.com>
2021-11-01 17:03:35 +0000

    not-an-endpoint error covered (tested passed)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,21 +8,21 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("app", ()=>{
-    it("GET /api/not-an-endpoint", ()=>{
-        return request(app).get("/api/not-an-endpoint").expect(404).then(({body}) => {
+    it("api/not-an-endpoint", ()=>{
+        return request(app).get("/not-an-endpoint").expect(404).then(({body}) => {
             expect(body.msg).toBe("Path not found!")
         })
     })
 
-    describe("GET /api/topics", ()=>{
-        it("should return status 200 and an object with key name : array of objects", ()=>{
-            return request(app).get("/api/topics").expect(200).then(({body})=>{
-                expect(body.topics.length).toBe(3)
-                body.topics.forEach((topic)=>{
-                    expect(topic).toEqual(
-                        expect.objectContaining({
-                            slug: expect.any(String),
-                            description: expect.any(String)
+describe("GET /api/topics", ()=>{
+    it("should return status 200 and an object with key name : array of objects", ()=>{
+        return request(app).get("/api/topics").expect(200).then(({body})=>{
+            expect(body.topics.length).toBe(3)
+            body.topics.forEach((topic)=>{
+                expect(topic).toEqual(
+                    expect.objectContaining({
+                        slug: expect.any(String),
+                        description: expect.any(String)
                         })
                     )
                 })
--- app.js
+++ app.js
@@ -4,4 +4,9 @@ const apiRouter = require("./routers/api-router")
 
 app.use("/api", apiRouter)
 
+
+app.all("/*", (req, res) => {
+    res.status(404).send({msg: "Path not found!"})
+});
+
 module.exports = app
